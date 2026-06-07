30ebe9def49e9f146c9adb2c7e026baf0edc78ea
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-07 14:55:52 +0400

    Create class DeleteTaskDemo + create main method inside the class

---
+++ src/main/java/com/example/Practice/Springboot/APIs7/DeleteTaskDemo.java
@@ -0,0 +1,40 @@
+package com.example.Practice.Springboot.APIs7;
+
+import java.util.ArrayList;
+
+public class DeleteTaskDemo {
+    public static void main(String[] args) {
+
+        ArrayList<String> tasks = new ArrayList<>();
+
+        tasks.add("Buy groceries");
+        tasks.add("Call doctor");
+        tasks.add("Finish report");
+
+        System.out.println("Initial Task List : ");
+        for(String task: tasks){
+            System.out.println("* " + task);
+        }
+
+        String targetTask = "Finish report";
+        System.out.println("Target Task: " +targetTask );
+
+        boolean found = false;
+
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).equalsIgnoreCase(targetTask)) {
+                tasks.remove(i);
+                System.out.println("Task "+targetTask+ " deleted successfully.");
+                found = true;
+                break;
+            }
+
+        }
+        if (!found) {
+            System.out.println("Task not found, no deletion performed ");
+
+        }
+        System.out.println("Final task list : " + tasks);
+
+    }
+}
