bd79479a4c7f898ca48b8e120136b87fa160a3c1
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-07 16:37:02 +0400

    Complete task 7

---
+++ src/main/java/com/example/Practice/Springboot/APIs7/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.example.Practice.Springboot.APIs7;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private RegistrationService registrationService;
+
+    @DeleteMapping("/registrations")
+    public String deleteRegistrations(
+            @RequestParam List<Integer> ids) {
+
+        return registrationService.deleteMultipleByIds(ids);
+    }
+}
---
+++ src/main/java/com/example/Practice/Springboot/APIs7/RegistrationService.java
@@ -0,0 +1,52 @@
+package com.example.Practice.Springboot.APIs7;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class RegistrationService {
+
+    private Map<Integer, EventRegistration> registrations = new HashMap<>();
+
+    @PostConstruct
+    public void InitializeData() {
+        registrations.put(101, new EventRegistration(101, "Ahmed"));
+        registrations.put(102, new EventRegistration(102, "Sara"));
+        registrations.put(103, new EventRegistration(103, "Ali"));
+        registrations.put(104, new EventRegistration(104, "Salim"));
+        registrations.put(105, new EventRegistration(105, "Fatima"));
+    }
+
+    public String deleteMultipleByIds(List<Integer> ids) {
+
+        List<Integer> deletedIds = new ArrayList<>();
+        List<Integer> notFoundIds = new ArrayList<>();
+
+        for (Integer id : ids) {
+            if (registrations.containsKey(id)) {
+                registrations.remove(id);
+                deletedIds.add(id);
+            } else {
+                notFoundIds.add(id);
+            }
+        }
+        return """
+                 Bulk Deletion Completed
+                 Total Requested: %d
+                 Deleted Successfully: %d
+                 Deleted IDs: %s
+                 Not Found IDs: %s
+                """
+                .formatted(
+                        ids.size(),
+                        deletedIds.size(),
+                        deletedIds,
+                        notFoundIds
+                );
+    }
+}
